b7c0def6043e12ea36ca60d0b781201440e9f1b5
david <reyna_b@upeu.edu.pe>
2020-04-26 21:57:17 -0500

    mi primer commit

---
+++ src/app/App.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Operaciones op = new Operaciones();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("X: ");
+        double x = sc.nextDouble();
+        System.out.println("Y: ");
+        double y = sc.nextDouble();
+        System.out.println("La suma es: "+op.suma(x, y));
+        System.out.println("La resta es: "+op.resta(x, y));
+        System.out.println("La multiplicación es: "+op.multiplicacion(x, y));
+        System.out.println("La division es: "+op.division(x, y));
+        sc.close();
+    }
+}
---
+++ src/app/Operaciones.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Operaciones {
+    double suma(double a, double b){
+        return a+b;
+    }
+    double resta(double a, double b){
+        return a-b;
+    }
+    double multiplicacion(double a, double b){
+        return a*b;
+    }
+    double division(double a, double b){
+        if(b==0)
+            return 0;
+        return a/b;
+    }
+}
